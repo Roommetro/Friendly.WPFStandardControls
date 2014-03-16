5b1a9e29de08f148095fb498fffcac9ea7e43b24
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-16 16:00:01 +0900

    WindowsAppFriendを保持するように変更

--- Project/RM.Friendly.WPFStandardControls3/WPFControlsBase.cs
+++ Project/RM.Friendly.WPFStandardControls3/WPFControlsBase.cs
@@ -1,4 +1,5 @@
 ﻿using Codeer.Friendly;
+using Codeer.Friendly.Windows;
 
 namespace RM.Friendly.WPFStandardControls3
 {
@@ -7,14 +8,20 @@ namespace RM.Friendly.WPFStandardControls3
     /// </summary>
     public class WPFControlsBase : IAppVarOwner
     {
+        public WindowsAppFriend App
+        {
+            get;
+            private set;
+        }
         public AppVar AppVar
         {
             get;
             private set;
         }
 
-        protected WPFControlsBase(AppVar appVar)
+        protected WPFControlsBase(WindowsAppFriend app, AppVar appVar)
         {
+            App = app;
             AppVar = appVar;
         }
     }
