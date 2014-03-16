8ae2c5169e8f64b58236462aa2bca4cba19e1c0c
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-16 17:04:27 +0900

    DataGridテスト作成

--- Project/Test/WPFDataGridTest.cs.cs
+++ Project/Test/WPFDataGridTest.cs.cs
@@ -30,8 +30,8 @@ namespace Test
             grid.Children.Add(dataGrid);
             dataGrid.ItemsSource = new Item[]
             {
-                new Item(){ Name = "山田", Age = 30},
-                new Item(){ Name = "鈴木", Age = 31},
+                new Item(){ Name = "山田", Language = ProgramingLanguage.C, IsActive = false },
+                new Item(){ Name = "鈴木", Language = ProgramingLanguage.CPP, IsActive = true },
             };
             return dataGrid;
         }
@@ -43,17 +43,41 @@ namespace Test
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestEmulateChangeCellText()
         {
             WPFDataGrid dataGrid = new WPFDataGrid(app, dataGridCore);
             dataGrid.EmulateChangeCellText(0, 0, "佐藤");
             Assert.AreEqual("佐藤", (string)dataGrid.Dynamic().ItemsSource[0].Name);
         }
+
+        [TestMethod]
+        public void TestEmulateChangeCellComboSelect()
+        {
+            WPFDataGrid dataGrid = new WPFDataGrid(app, dataGridCore);
+            dataGrid.EmulateChangeCellComboSelect(0, 1, 2);
+            Assert.AreEqual(ProgramingLanguage.CSP, (ProgramingLanguage)dataGrid.Dynamic().ItemsSource[0].Language);
+        }
+
+        [TestMethod]
+        public void TestEmulateCellCheck()
+        {
+            WPFDataGrid dataGrid = new WPFDataGrid(app, dataGridCore);
+            dataGrid.EmulateCellCheck(0, 2, true);
+            Assert.AreEqual(true, (bool)dataGrid.Dynamic().ItemsSource[0].IsActive);
+        }
+    }
+
+    public enum ProgramingLanguage
+    {
+        C,
+        CPP,
+        CSP
     }
 
     public class Item
     {
         public string Name { get; set; }
-        public int Age { get; set; }
+        public ProgramingLanguage Language { get; set; }
+        public bool IsActive { get; set; }
     }
 }
