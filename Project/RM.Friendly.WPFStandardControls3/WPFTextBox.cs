aa27fbdca21eb8407fc96641c23272a2748edd53
JLRishe <jlrishe@gmail.com>
2014-03-16 17:40:01 +0900

    WPFTextBoxのインタフェース、同期テスト

--- Project/RM.Friendly.WPFStandardControls3/WPFTextBox.cs
+++ Project/RM.Friendly.WPFStandardControls3/WPFTextBox.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
+using Codeer.Friendly;
+using Codeer.Friendly.Windows;
 
 namespace RM.Friendly.WPFStandardControls
 {
-    class WPFTextBox
+    public class WPFTextBox : WPFControlsBase
     {
+        public WPFTextBox(WindowsAppFriend app, AppVar appVar)
+            : base(app, appVar) { }
+
+        public void EmulateChangeText(string text)
+        {
+        }
+
+        public void EmulateChangeText(string text, Async async)
+        {
+        }
+
+        public string Text
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        private static void EmulateChangeTextInTarget(TextBox textBox, string value)
+        {
+            textBox.Text = value;
+        }
     }
 }
--- Project/Test/WPFTextBoxTest.cs
+++ Project/Test/WPFTextBoxTest.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Dynamic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RM.Friendly.WPFStandardControls;
 
 namespace Test
 {
     [TestClass]
     public class WPFTextBoxTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        WindowsAppFriend app;
+        dynamic mainWindow;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            app = new WindowsAppFriend(Process.Start("Target.exe"));
+            mainWindow = app.Type<Application>().Current.MainWindow;
+            WindowsAppExpander.LoadAssemblyFromFile(app, GetType().Assembly.Location);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (app != null)
+            {
+                app.Dispose();
+                Process process = Process.GetProcessById(app.ProcessId);
+                process.CloseMainWindow();
+                app = null;
+            }
+        }
+
+        const string TestValue = "It worked!";
+
+        [TestMethod]
+        public void TestEmulateChangeText()
+        {
+            WPFTextBox textBox = new WPFTextBox(app, mainWindow._textBox);
+            textBox.EmulateChangeText(TestValue);
+            string textBoxText = textBox.Text;
+            Assert.AreEqual(TestValue, textBoxText);
         }
     }
 }
