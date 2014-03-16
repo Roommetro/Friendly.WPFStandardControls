fc60d97a88feabd4ba6fbe440b92ce37adf5847a
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-17 00:25:51 +0900

    筧さん作業マージ

--- Project/RM.Friendly.WPFStandardControls3/WpfButtonBase.cs
+++ Project/RM.Friendly.WPFStandardControls3/WpfButtonBase.cs
@@ -1,8 +1,7 @@
 ﻿using Codeer.Friendly;
 using Codeer.Friendly.Windows;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
+using System.Windows.Controls.Primitives;
 
 namespace RM.Friendly.WPFStandardControls
 {
@@ -11,7 +10,49 @@ namespace RM.Friendly.WPFStandardControls
         public WPFButtonBase(WindowsAppFriend app, AppVar appVar)
             : base(app, appVar)
         {
+        }
 
+#if ENG
+        /// <summary>
+        /// Performs a click.
+        /// </summary>
+#else
+        /// <summary>
+        /// クリックです。
+        /// </summary>
+#endif
+        public void EmulateClick()
+        {
+            App[GetType(), "EmulateClickInTarget"](AppVar);
+        }
+
+#if ENG
+        /// <summary>
+        /// Performs a click.
+        /// Executes asynchronously. 
+        /// </summary>
+        /// <param name="async">Asynchronous execution.</param>
+#else
+        /// <summary>
+        /// クリックです。
+        /// 非同期で実行します。
+        /// </summary>
+        /// <param name="async">非同期実行オブジェクト。</param>
+#endif
+        public void EmulateClick(Async async)
+        {
+            App[GetType(), "EmulateClickInTarget", async](AppVar);
+        }
+
+        /// <summary>
+        /// クリックです。
+        /// </summary>
+        /// <param name="button">ボタン。</param>
+        static void EmulateClickInTarget(ButtonBase button)
+        {
+            button.Focus();
+            MethodInfo methodInfo = button.GetType().GetMethod("OnClick", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.InvokeMethod);
+            methodInfo.Invoke(button, new object[] { });
         }
     }
 }
--- Project/Test/WPFButtonBaseTest.cs
+++ Project/Test/WPFButtonBaseTest.cs
@@ -1,14 +1,93 @@
-﻿using System;
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.Friendly.Windows.NativeStandardControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RM.Friendly.WPFStandardControls;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace Test
 {
     [TestClass]
     public class WPFButtonBaseTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        WindowsAppFriend _app;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            _app = new WindowsAppFriend(Process.Start("Target.exe"));
+            WindowsAppExpander.LoadAssembly(_app, GetType().Assembly);
+        }
+
+        private class ButtonEventCheck
+        {
+            public bool ButtonClickCalled { get; set; }
+            public ButtonEventCheck(ButtonBase ButtonBase, bool showMessageBoxFlg)
+            {
+                RoutedEventHandler handler = (s, e) =>
+                {
+                    ButtonClickCalled = true;
+                    if (showMessageBoxFlg)
+                    {
+                        MessageBox.Show("TestMessageWindow");
+                    }
+                };
+
+                ButtonBase.Click += handler;
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).CloseMainWindow();
+        }
+
+        [TestMethod]
+        public void ButtonClickTest()
+        {
+            // Arrenge
+            dynamic main = _app.Type<Application>().Current.MainWindow;
+            AppVar buttonCore = _app.Type<Button>()();
+            main._grid.Children.Add(buttonCore);
+            dynamic checker = _app.Type<ButtonEventCheck>()(buttonCore, false);
+            WPFButtonBase ButtonBase = new WPFButtonBase(_app, buttonCore);
+
+            // Act
+            ButtonBase.EmulateClick();
+            Assert.IsTrue((bool)checker.ButtonClickCalled);
+        }
+
+        [TestMethod]
+        public void ButtonClickAsyncTest()
+        {
+            // Arrenge
+            dynamic main = _app.Type<Application>().Current.MainWindow;
+            AppVar buttonCore = _app.Type<Button>()();
+            main._grid.Children.Add(buttonCore);
+            dynamic checker = _app.Type<ButtonEventCheck>()(buttonCore, true);
+            WindowControl windowControl = WindowControl.FromZTop(_app);
+            WPFButtonBase ButtonBase = new WPFButtonBase(_app, buttonCore);
+
+            // Act
+            Async async = new Async();
+            ButtonBase.EmulateClick(async);
+
+            // Assert
+            WindowControl messageBoxControl = windowControl.WaitForNextModal();
+            NativeMessageBox messageBox = new NativeMessageBox(messageBoxControl);
+            Assert.AreEqual("TestMessageWindow", messageBox.Message);
+            Assert.IsTrue((bool)checker.ButtonClickCalled);
+
+            // Teardown
+            messageBox.EmulateButtonClick("OK");
+            messageBoxControl.WaitForDestroy();
+            async.WaitForCompletion();
         }
     }
 }
