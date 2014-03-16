56706f7e3022a4a6960e0d4af18ce4bea15bd3d4
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-16 16:32:10 +0900

    テスト追加

---
+++ Project/Test/WPFDataGridTest.cs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+
+namespace Test
+{
+    [TestClass]
+    public class WPFDataGridTest
+    {
+        WindowsAppFriend app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            app = new WindowsAppFriend(Process.Start("Target.exe"));
+
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(app.ProcessId).CloseMainWindow();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
