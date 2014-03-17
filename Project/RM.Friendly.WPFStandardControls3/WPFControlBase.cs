0a6f33e93bca96dc63ae6dc53a77779c64a08157
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-18 00:20:00 +0900

    共通メソッド追加

--- Project/RM.Friendly.WPFStandardControls3/WPFControlBase.cs
+++ Project/RM.Friendly.WPFStandardControls3/WPFControlBase.cs
@@ -1,5 +1,6 @@
 ﻿using Codeer.Friendly;
 using Codeer.Friendly.Windows;
+using System.Windows;
 
 namespace RM.Friendly.WPFStandardControls
 {
@@ -29,5 +30,35 @@ namespace RM.Friendly.WPFStandardControls
 #endif
         protected WPFControlBase(WindowsAppFriend app, AppVar appVar)
             : base(app, appVar) { }
+
+#if ENG
+        /// <summary>
+        /// Returns true if the item is set to visible.
+        /// </summary>
+#else
+        /// <summary>
+        /// 表示/非表示を取得します。
+        /// </summary>
+#endif
+        public Visibility Visibility { get { return this.GetPropValue<Visibility>(); } }
+
+#if ENG
+        /// <summary>
+        /// Returns true if the control is enabled.
+        /// </summary>
+#else
+        /// <summary>
+        /// 活性/非活性を取得します。
+        /// </summary>
+#endif
+        public bool IsEnabled { get { return this.GetPropValue<bool>(); } }
+
+        /// <summary>
+        /// フォーカスを当てます。
+        /// </summary>
+        public void Focus()
+        {
+            this["Focus"]();
+        }
     }
 }
