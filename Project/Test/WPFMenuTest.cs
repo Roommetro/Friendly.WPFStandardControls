c06f765c4e8aabf06fbba1546070b4c89a404a93
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-04-09 01:01:22 +0900

    ハッカソン準備

---
+++ Project/Test/WPFMenuTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class WPFMenuTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Project/Test/WPFTreeViewTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class WPFTreeViewTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
