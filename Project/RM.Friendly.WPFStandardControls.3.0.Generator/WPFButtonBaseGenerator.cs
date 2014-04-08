b97a4cbced612b4f475a3ac83e24b3fa0ad4b581
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-04-09 00:10:38 +0900

    ジェネレータ作成準備

---
+++ Project/RM.Friendly.WPFStandardControls.3.0.Generator/WPFButtonBaseGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Codeer.TestAssistant.GeneratorToolKit;
+using System.Windows.Controls;
+
+namespace RM.Friendly.WPFStandardControls.Generator
+{
+    /// <summary>
+    /// コード生成
+    /// </summary>
+    public class WPFButtonBaseGenerator : GeneratorBase
+    {
+        Button _control;
+
+        /// <summary>
+        /// アタッチ。
+        /// </summary>
+        protected override void Attach()
+        {
+            _control = (Button)ControlObject;
+            _control.Click += ButtonClick;
+        }
+
+        /// <summary>
+        /// ディタッチ。
+        /// </summary>
+        protected override void Detach()
+        {
+            _control.Click -= ButtonClick;
+        }
+
+        /// <summary>
+        /// ボタン押下
+        /// </summary>
+        /// <param name="sender">イベント送信元</param>
+        /// <param name="e">イベント内容</param>
+        void ButtonClick(object sender, EventArgs e)
+        {
+            if (_control.IsFocused)
+            {
+                AddSentence(new TokenName(), ".EmulateClick(", new TokenAsync(CommaType.Non), ");");
+            }
+        }
+    }
+}
---
+++ Project/RM.Friendly.WPFStandardControls.4.0.Generator/WPFDataGridGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace RM.Friendly.WPFStandardControls.Generator
+{
+    /// <summary>
+    /// コード生成
+    /// </summary>
+    public class WPFDataGridGenerator : GeneratorBase
+    {
+        /// <summary>
+        /// アタッチ。
+        /// </summary>
+        protected override void Attach()
+        {
+        }
+
+        /// <summary>
+        /// ディタッチ。
+        /// </summary>
+        protected override void Detach()
+        {
+        }
+
+        /// <summary>
+        /// ボタン押下
+        /// </summary>
+        /// <param name="sender">イベント送信元</param>
+        /// <param name="e">イベント内容</param>
+        void ButtonClick(object sender, EventArgs e)
+        {
+        }
+    }
+}
