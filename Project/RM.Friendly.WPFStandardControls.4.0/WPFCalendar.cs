ad5b2caac725ae14eb20d5270c91981715b1c99d
JLRishe <jlrishe@gmail.com>
2014-04-09 20:53:28 +0900

    WPFCalendarのEmulateSelectDateと.SelectedDate

---
+++ Project/RM.Friendly.WPFStandardControls.4.0/WPFCalendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Codeer.Friendly;
+
+namespace RM.Friendly.WPFStandardControls
+{
+    public class WPFCalendar : WPFControlBase4
+    {
+        public WPFCalendar(AppVar appVar)
+            : base(appVar) { }
+
+        public DateTime? SelectedDate
+        {
+            get { return Getter<DateTime?>("SelectedDate"); }
+        }
+
+        public void EmulateChangeDate(DateTime? date)
+        {
+            InvokeStatic("EmulateChangeDate", date);
+        }
+
+        private static void EmulateChangeDate(Calendar calendar, DateTime? date)
+        {
+            calendar.SelectedDate = date;
+        }
+    }
+}
