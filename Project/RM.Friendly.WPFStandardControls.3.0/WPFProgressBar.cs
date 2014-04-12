912655cf80f97b72543c92e38bf9e5def4d951ab
Hidari0415 <ika5140@gmail.com>
2014-04-12 21:46:10 +0900

    WPFProgressBarの実装、およびそのTestを追加。

--- Project/RM.Friendly.WPFStandardControls.3.0/WPFProgressBar.cs
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFProgressBar.cs
@@ -1,10 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Controls;
+using Codeer.Friendly;
 
 namespace RM.Friendly.WPFStandardControls
 {
-    class WPFProgressBar
+    /// <summary>
+    /// TypeがSystem.Windows.Controls.ProgressBarのウィンドウに対応した操作を提供します。
+    /// </summary>
+    public class WPFProgressBar : WPFControlBase
     {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="appVar">アプリケーション内変数。</param>
+        public WPFProgressBar(AppVar appVar)
+            : base(appVar){ }
+
+        /// <summary>
+        /// 値を取得します。
+        /// </summary>
+        public double Value
+        {
+            get { return Getter<double>("Value"); }
+        }
+
+        /// <summary>
+        /// 最小値を取得します。
+        /// </summary>
+        public double Minimum
+        {
+            get { return Getter<double>("Minimum"); }
+        }
+
+        /// <summary>
+        /// 最大値を取得します。
+        /// </summary>
+        public double Maximum
+        {
+            get { return Getter<double>("Maximum"); }
+        }
+
+        /// <summary>
+        /// 値を変更します。
+        /// </summary>
+        /// <param name="value">値</param>
+        public void EmulateChangeValue(double value)
+        {
+            InvokeStatic("EmulateChangeValue", value);
+        }
+
+        /// <summary>
+        /// 値を変更します。
+        /// 非同期で実行します。
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="async">非同期実行オブジェクト</param>
+        public void EmulateChangeValue(double value, Async async)
+        {
+            InvokeStatic("EmulateChangeValue", async, value);
+        }
+        static void EmulateChangeValue(ProgressBar progressBar, double value)
+        {
+            progressBar.Focus();
+            progressBar.Value = value;
+        }
+
     }
 }
--- Project/Test/WPFProgressBarTest.cs
+++ Project/Test/WPFProgressBarTest.cs
@@ -1,14 +1,91 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.Friendly.Windows.NativeStandardControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RM.Friendly.WPFStandardControls;
 
 namespace Test
 {
     [TestClass]
     public class WPFProgressBarTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        WindowsAppFriend _app;
+        WindowControl _mainWindow;
+
+        dynamic _target;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            _app = new WindowsAppFriend(Process.Start("Target.exe"));
+            _mainWindow = WindowControl.FromZTop(_app);
+
+            dynamic win = _app.Type<Application>().Current.MainWindow;
+            dynamic grid = win._grid;
+            _target = _app.Type<ProgressBar>()();
+            grid.Children.Add(_target);
+
+            WindowsAppExpander.LoadAssemblyFromFile(_app, GetType().Assembly.Location);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (_app != null)
+            {
+                _app.Dispose();
+                Process process = Process.GetProcessById(_app.ProcessId);
+                process.CloseMainWindow();
+                _app = null;
+            }
+        }
+
+        const double TestValue = 10;
+
+        [TestMethod]
+        public void TestEmulateChangeValue()
+        {
+            var progressBar = new WPFProgressBar(_target);
+            progressBar.EmulateChangeValue(TestValue);
+            Assert.AreEqual(TestValue, progressBar.Value);
+        }
+
+        [TestMethod]
+        public void TestEmulateChangeValueAsync()
+        {
+            var progressBar = new WPFProgressBar(_target);
+            _app[GetType(), "ChangeValueEvent"](progressBar.AppVar);
+            var async = new Async();
+            progressBar.EmulateChangeValue(TestValue, async);
+            new NativeMessageBox(_mainWindow.WaitForNextModal()).EmulateButtonClick("OK");
+            async.WaitForCompletion();
+            Assert.AreEqual(TestValue, progressBar.Value);
+        }
+
+        static void ChangeValueEvent(ProgressBar progressBar)
+        {
+            progressBar.ValueChanged += (s, e) => MessageBox.Show("");
+        }
+
+        [TestMethod]
+        public void TestMinimum()
+        {
+            var progressBar = new WPFProgressBar(_target);
+            progressBar.Dynamic().Minimum = (double)10.5;
+            Assert.AreEqual(10.5, progressBar.Minimum);
+        }
+
+        [TestMethod]
+        public void TestMaximum()
+        {
+            var progressBar = new WPFProgressBar(_target);
+            progressBar.Dynamic().Maximum = (double)10.5;
+            Assert.AreEqual(10.5, progressBar.Maximum);
         }
     }
 }
