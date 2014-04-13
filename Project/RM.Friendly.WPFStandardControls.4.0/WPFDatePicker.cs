9348839aa4937b4b301184fa19eb21f94b411f25
Kazaoka <hkazaoka@gmail.com>
2014-04-14 03:23:45 +0900

    DatePicker追加

---
+++ Project/RM.Friendly.WPFStandardControls.4.0/WPFDatePicker.cs
@@ -0,0 +1,33 @@
+﻿using Codeer.Friendly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Codeer.Friendly.Dynamic;
+
+namespace RM.Friendly.WPFStandardControls
+{
+    public class WPFDatePicker : WPFControlBase4
+    {
+        public WPFDatePicker(AppVar appVar)
+            : base(appVar) { }
+
+        public DateTime SelectedDate
+        {
+            get { return Getter<DateTime>("SelectedDate"); }
+        }
+        public void EmulateChangeSelectedDate(DateTime datetime)
+        {
+            InvokeStatic("EmulateChangeSelectedDate", datetime);
+        }
+        public void EmulateChangeSelectedDate(DateTime datetime, Async async)
+        {
+            InvokeStatic("EmulateChangeSelectedDate", async, datetime);
+        }
+        static void EmulateChangeSelectedDate(DatePicker datepicker, DateTime datetime)
+        {
+            datepicker.Focus();
+            datepicker.SelectedDate = datetime;
+        }
+    }
+}
---
+++ Project/Test/WPFDatePickerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows.Grasp;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using Codeer.Friendly.Windows.NativeStandardControls;
+using RM.Friendly.WPFStandardControls;
+using Codeer.Friendly;
+
+namespace Test
+{
+    [TestClass]
+    public class WPFDatePickerTest : WPFTestBase<DatePicker>
+    {
+        DateTime TestValue = new DateTime(2000,1,14);
+
+        [TestMethod]
+        public void TestEmulateChangeSelectedDate()
+        {
+            var datepicker = new WPFDatePicker(Target);
+            datepicker.EmulateChangeSelectedDate(TestValue);
+            Assert.AreEqual(TestValue, datepicker.SelectedDate);
+        }
+
+        [TestMethod]
+        public void TestEmulateChangeSelectedDateAsync()
+        {
+            var datepicker = new WPFDatePicker(Target);
+            CallRemoteMethod("AttachChangeDateHandler", datepicker);
+
+            datepicker.EmulateChangeSelectedDate(TestValue, new Async());
+            ClickNextMessageBox();
+            Assert.AreEqual(TestValue, datepicker.SelectedDate);
+        }
+
+        static void ChangeSelectedDateEvent(DatePicker datepicker)
+        {
+            datepicker.SelectedDateChanged += (s, e) => MessageBox.Show("");
+        }
+
+        [TestMethod]
+        public void TestSelectedDate()
+        {
+            var datepicker = new WPFDatePicker(Target);
+            var testvalue = new DateTime(1970, 5, 2);
+            datepicker.Dynamic().SelectedDate = testvalue;
+            Assert.AreEqual(testvalue, datepicker.SelectedDate);
+        }
+        static void AttachChangeDateHandler(DatePicker datepicker)
+        {
+            EventHandler<SelectionChangedEventArgs> handler = null;
+            handler = (s, e) =>
+                {
+                    MessageBox.Show("");
+                    datepicker.SelectedDateChanged -= handler;
+                };
+
+            datepicker.SelectedDateChanged += handler;
+        }
+    }
+}
