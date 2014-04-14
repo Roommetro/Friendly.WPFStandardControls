b9a348c0f99f9731313e7661dd52488a64b9b325
Hidari0415 <ika5140@gmail.com>
2014-04-15 08:41:40 +0900

    WPFProgressBarクラスに英語のコメントを追加しました。

--- Project/RM.Friendly.WPFStandardControls.3.0/WPFProgressBar.cs
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFProgressBar.cs
@@ -3,37 +3,68 @@ using Codeer.Friendly;
 
 namespace RM.Friendly.WPFStandardControls
 {
+#if ENG
+    /// <summary>
+    /// Provides operations on controls of type System.Windows.Controls.ProgressBar.
+    /// </summary>
+#else
     /// <summary>
     /// TypeがSystem.Windows.Controls.ProgressBarのウィンドウに対応した操作を提供します。
     /// </summary>
+#endif
     public class WPFProgressBar : WPFControlBase
     {
+#if ENG
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="appVar">Application variable object for the control.</param>
+#else
         /// <summary>
         /// コンストラクタ
         /// </summary>
         /// <param name="appVar">アプリケーション内変数。</param>
+#endif
         public WPFProgressBar(AppVar appVar)
             : base(appVar){ }
 
+#if ENG
+        /// <summary>
+        /// Returns the control's value.
+        /// </summary>
+#else
         /// <summary>
         /// 値を取得します。
         /// </summary>
+#endif
         public double Value
         {
             get { return Getter<double>("Value"); }
         }
 
+#if ENG
+        /// <summary>
+        /// Returns the control's minimum value.
+        /// </summary>
+#else
         /// <summary>
         /// 最小値を取得します。
         /// </summary>
+#endif
         public double Minimum
         {
             get { return Getter<double>("Minimum"); }
         }
 
+#if ENG
+        /// <summary>
+        /// Returns the control's maximum value.
+        /// </summary>
+#else
         /// <summary>
         /// 最大値を取得します。
         /// </summary>
+#endif
         public double Maximum
         {
             get { return Getter<double>("Maximum"); }
