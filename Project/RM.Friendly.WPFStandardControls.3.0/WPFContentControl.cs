daca31118e82b37a2a5a2fa2c878e13bedaa5577
石川達也 <stone.guitar.777@gmail.com>
2015-12-15 22:29:08 +0900

    WPFContentControl追加

---
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFContentControl.cs
@@ -0,0 +1,42 @@
+﻿using Codeer.Friendly;
+using System.Windows.Controls;
+
+namespace RM.Friendly.WPFStandardControls
+{
+#if ENG
+    /// <summary>
+    /// Provides operations on controls of type System.Windows.Controls.ContentControl.
+    /// </summary>
+#else
+    /// <summary>
+    /// TypeがSystem.Windows.Controls.ContentControlに対応した操作を提供します。
+    /// </summary>
+#endif
+    public class WPFContentControl : WPFControlBase<ContentControl>
+    {
+#if ENG
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="appVar">Application variable object for the control.</param>
+#else
+        /// <summary>
+        /// コンストラクタです。
+        /// </summary>
+        /// <param name="appVar">アプリケーション内変数。</param>
+#endif
+        public WPFContentControl(AppVar appVar)
+            : base(appVar) { }
+
+#if ENG
+        /// <summary>
+        /// Get Content.
+        /// </summary>
+#else
+        /// <summary>
+        /// コンテンツを取得します。
+        /// </summary>
+#endif
+        public AppVar Content { get { return this["Content"](); } }
+    }
+}
---
+++ Project/Test/WPFContentControlTest.cs
@@ -0,0 +1,39 @@
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.Friendly.Windows.NativeStandardControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RM.Friendly.WPFStandardControls;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Test
+{
+    [TestClass]
+    public class WPFContentControlTest
+    {
+        WindowsAppFriend _app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _app = new WindowsAppFriend(Process.Start("Target.exe"));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).CloseMainWindow();
+        }
+
+        [TestMethod]
+        public void ContentTest()
+        {
+            string fullName = new WPFContentControl(_app.Type<Application>().Current.MainWindow).Content.Dynamic().GetType().FullName;
+            Assert.AreEqual(typeof(Grid).FullName, fullName);
+        }
+    }
+}
