4b87213721670d6eb8a03ea21926134b6ff35e2e
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-11-24 20:16:04 +0900

    vrsion up.

--- Project/RM.Friendly.WPFStandardControls.3.0/WPFControlBase.cs
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFControlBase.cs
@@ -1,7 +1,10 @@
 ﻿using Codeer.Friendly;
 using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace RM.Friendly.WPFStandardControls
 {
@@ -14,7 +17,7 @@ namespace RM.Friendly.WPFStandardControls
     /// WPFのコントロールを操作するためのクラスの基本クラスです。
     /// </summary>
 #endif
-    public class WPFControlBase<CoreType> : AppVarWrapper<CoreType> where CoreType : UIElement
+    public class WPFControlBase<CoreType> : AppVarWrapper<CoreType>, IUIObject where CoreType : UIElement
     {
 #if ENG
         /// <summary>
@@ -51,5 +54,58 @@ namespace RM.Friendly.WPFStandardControls
         /// </summary>
 #endif
         public bool IsEnabled { get { return Getter<bool>("IsEnabled"); } }
+
+#if ENG
+        /// <summary>
+        /// Returns the size of IUIObject.
+        /// </summary>
+#else
+        /// <summary>
+        /// IUIObjectのサイズを取得します。
+        /// </summary>
+#endif
+        public System.Drawing.Size Size
+        {
+            get
+            {
+                var size = Getter<Size>("RenderSize");
+                return new System.Drawing.Size((int)size.Width, (int)size.Height);
+            }
+        }
+        
+#if ENG
+        /// <summary>
+        /// Make it active.
+        /// </summary>
+#else
+        /// <summary>
+        /// アクティブな状態にします。
+        /// </summary>
+#endif
+        public void Activate()
+        {
+            var source = App[typeof(HwndSource), "FromVisual"](this);
+            new WindowControl(App, (IntPtr)source["Handle"]().Core).Activate();
+            AppVar["Focus"]();
+        }
+
+#if ENG
+        /// <summary>
+        /// Convert IUIObject's client coordinates to screen coordinates.
+        /// </summary>
+        /// <param name="clientPoint">client coordinates.</param>
+        /// <returns>screen coordinates.</returns>
+#else
+        /// <summary>
+        /// IUIObjectのクライアント座標からスクリーン座標に変換します。
+        /// </summary>
+        /// <param name="clientPoint">クライアント座標</param>
+        /// <returns>スクリーン座標</returns>
+#endif
+        public System.Drawing.Point PointToScreen(System.Drawing.Point clientPoint)
+        {
+            var pos = (Point)AppVar["PointToScreen"](new Point(clientPoint.X, clientPoint.Y)).Core;
+            return new System.Drawing.Point((int)pos.X, (int)pos.Y);
+        }
     }
 }
--- Project/Test/WPFControlBaseTest.cs
+++ Project/Test/WPFControlBaseTest.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Linq;
 using RM.Friendly.WPFStandardControls;
+using Codeer.Friendly.Windows.Grasp;
 
 namespace Test
 {
@@ -36,7 +37,6 @@ namespace Test
         [TestMethod]
         public void TestVisibility()
         {
-            var win = app.Type(typeof(Application)).Current.MainWindow;
             var selector = new WPFListBox(target);
             Assert.AreEqual(Visibility.Visible, selector.Visibility);
             selector.Dynamic().Visibility = Visibility.Hidden;
@@ -46,11 +46,40 @@ namespace Test
         [TestMethod]
         public void TestIsEnabled()
         {
-            var win = app.Type(typeof(Application)).Current.MainWindow;
             var selector = new WPFListBox(target);
             Assert.IsTrue(selector.IsEnabled);
             selector.Dynamic().IsEnabled = false;
             Assert.IsFalse(selector.IsEnabled);
+        }
+
+        [TestMethod]
+        public void TestActivate()
+        {
+            var win = new WindowControl(app.Type(typeof(Application)).Current.MainWindow);
+            var newWin = app.Type<Window>()();
+            newWin.Show();
+            var selector = new WPFListBox(target);
+            selector.Activate();
+
+            Assert.AreEqual(WindowControl.FromZTop(app).Handle, win.Handle);
+            Assert.IsTrue((bool)selector.Dynamic().IsFocused);
+            newWin.Close();
+        }
+
+        [TestMethod]
+        public void TestSize()
+        {
+            var selector = new WPFListBox(target);
+            Size size = selector.Dynamic().RenderSize;
+            Assert.AreEqual(new System.Drawing.Size((int)size.Width, (int)size.Height), selector.Size);
+        }
+
+        [TestMethod]
+        public void TestPointToScreen()
+        {
+            var selector = new WPFListBox(target);
+            Point size = selector.Dynamic().PointToScreen(new Point());
+            Assert.AreEqual(new System.Drawing.Point((int)size.X, (int)size.Y), selector.PointToScreen(new System.Drawing.Point()));
         }
     }
 }
