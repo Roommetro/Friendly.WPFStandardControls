b9cdeaeec0b022a430235e4242efb734cf16d0b6
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-07-25 16:28:38 +0900

    Add WPFUserControl.

--- Project/RM.Friendly.WPFStandardControls.3.0/WPFContentControl.cs
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFContentControl.cs
@@ -13,7 +13,44 @@ namespace RM.Friendly.WPFStandardControls
     /// TypeがSystem.Windows.Controls.ContentControlに対応した操作を提供します。
     /// </summary>
 #endif
-    public class WPFContentControl : WPFControlBase<ContentControl>
+    public class WPFContentControlCore<T> : WPFControlBase<T> where T : ContentControl
+    {
+#if ENG
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="appVar">Application variable object for the control.</param>
+#else
+        /// <summary>
+        /// コンストラクタです。
+        /// </summary>
+        /// <param name="appVar">アプリケーション内変数。</param>
+#endif
+        public WPFContentControlCore(AppVar appVar)
+            : base(appVar) { }
+
+#if ENG
+        /// <summary>
+        /// Get Content.
+        /// </summary>
+#else
+        /// <summary>
+        /// コンテンツを取得します。
+        /// </summary>
+#endif
+        public AppVar Content { get { return this["Content"](); } }
+    }
+
+#if ENG
+    /// <summary>
+    /// Provides operations on controls of type System.Windows.Controls.ContentControl.
+    /// </summary>
+#else
+    /// <summary>
+    /// TypeがSystem.Windows.Controls.ContentControlに対応した操作を提供します。
+    /// </summary>
+#endif
+    public class WPFContentControl : WPFContentControlCore<ContentControl>
     {
 #if ENG
         /// <summary>
@@ -28,16 +65,5 @@ namespace RM.Friendly.WPFStandardControls
 #endif
         public WPFContentControl(AppVar appVar)
             : base(appVar) { }
-
-#if ENG
-        /// <summary>
-        /// Get Content.
-        /// </summary>
-#else
-        /// <summary>
-        /// コンテンツを取得します。
-        /// </summary>
-#endif
-        public AppVar Content { get { return this["Content"](); } }
     }
 }
---
+++ Project/RM.Friendly.WPFStandardControls.3.0/WPFUserControl.cs
@@ -0,0 +1,32 @@
+﻿using Codeer.Friendly;
+using Codeer.TestAssistant.GeneratorToolKit;
+using System.Windows.Controls;
+
+namespace RM.Friendly.WPFStandardControls
+{
+#if ENG
+    /// <summary>
+    /// Provides operations on controls of type System.Windows.Controls.UserControl.
+    /// </summary>
+#else
+    /// <summary>
+    /// TypeがSystem.Windows.Controls.UserControl。
+    /// </summary>
+#endif
+    public class WPFUserControl : WPFContentControlCore<UserControl>
+    {
+#if ENG
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="appVar">Application variable object for the control.</param>
+#else
+        /// <summary>
+        /// コンストラクタです。
+        /// </summary>
+        /// <param name="appVar">アプリケーション内変数。</param>
+#endif
+        public WPFUserControl(AppVar appVar)
+            : base(appVar) { }
+    }
+}
