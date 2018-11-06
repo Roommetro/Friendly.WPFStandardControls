fc3c8ca1eeee29b05d9ca7e00d66956387af43d9
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-11-06 23:47:31 +0900

    Bug Fix Capture Generator of ListBox, ListView, TabControl.

---
+++ Project/RM.Friendly.WPFStandardControls.3.0.Generator/WPFTabControlGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Controls.Primitives;
+using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace RM.Friendly.WPFStandardControls.Generator
+{
+    [CaptureCodeGenerator("RM.Friendly.WPFStandardControls.WPFTabControl")]
+    public class WPFTabControlGenerator : CaptureCodeGeneratorBase
+    {
+        Selector _control;
+
+        protected override void Attach()
+        {
+            _control = (Selector)ControlObject;
+            _control.SelectionChanged += SelectionChanged;
+        }
+
+        protected override void Detach()
+        {
+            _control.SelectionChanged -= SelectionChanged;
+        }
+
+        void SelectionChanged(object sender, EventArgs e)
+        {
+            AddSentence(new TokenName(), ".EmulateChangeSelectedIndex(" + _control.SelectedIndex, new TokenAsync(CommaType.Before), ");");
+        }
+    }
+}
