37ac5c8c0086588f789cb63becdc853eea5975aa
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2023-07-06 15:50:42 +0900

    adjust vs path.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,4 +1,4 @@
 rd /s /q "../ReleaseBinary"
-"%DevEnvDir%devenv.exe" "../Friendly.WPFStandardControls.sln" /rebuild Release
-"%DevEnvDir%devenv.exe" "../Friendly.WPFStandardControls.sln" /rebuild Release-English
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe" "../Friendly.WPFStandardControls.sln" /rebuild Release
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe" "../Friendly.WPFStandardControls.sln" /rebuild Release-English
 nuget pack Friendly.WPFStandardControls.nuspec
