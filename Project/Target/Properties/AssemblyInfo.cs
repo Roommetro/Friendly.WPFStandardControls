0b3149e1a1239ebbabd755a15eb9fbf5f797fa2b
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2023-07-07 12:02:12 +0900

    version up.

--- Project/Target/Properties/AssemblyInfo.cs
+++ Project/Target/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,6 +51,6 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.59.1.0")]
+[assembly: AssemblyFileVersion("1.59.1.0")]
 [assembly: System.CLSCompliant(true)]
--- Project/Test/Properties/AssemblyInfo.cs
+++ Project/Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、以下のように '*' を使用してビルド番号とリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.59.1.0")]
+[assembly: AssemblyFileVersion("1.59.1.0")]
 [assembly: System.CLSCompliant(true)]
