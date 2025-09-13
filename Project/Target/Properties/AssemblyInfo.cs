c66d88c6af346629a3c9316a6339d179cfba0900
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-09-13 18:17:57 +0900

    version up

--- Project/Target/Properties/AssemblyInfo.cs
+++ Project/Target/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.60.0.0")]
-[assembly: AssemblyFileVersion("1.60.0.0")]
+[assembly: AssemblyVersion("1.60.1.0")]
+[assembly: AssemblyFileVersion("1.60.1.0")]
 [assembly: System.CLSCompliant(true)]
